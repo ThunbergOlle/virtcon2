0148a8b4c7339193a111844922b9c2f5978ac4a6
Olle Thunberg <olle@fls.se>
2023-05-08 11:24:17 +0200

    Fix start world

--- apps/packet_data_server/src/packet/packets/request_join_packet.ts
+++ apps/packet_data_server/src/packet/packets/request_join_packet.ts
@@ -12,14 +12,14 @@ export default async function request_join_packet(packet: NetworkPacketData<Requ
 
   if (!world.length) {
     log(`World ${packet.world_id} is not running. Starting up world...`, LogLevel.INFO, LogApp.PACKET_DATA_SERVER);
-    /* Create a new world in the redis database */
-    const new_world: RedisWorld = {
-      id: packet.world_id,
-      players: [],
-      buildings: [],
-    };
-    await worldService.startWorldProcess(new_world, redisPubClient);
+    try {
+      const loaded_world = await worldService.loadWorld(packet.world_id);
+      await worldService.startWorldProcess(loaded_world, redisPubClient);
+    } catch (e) {
+      return log(`Failed to start world ${packet.world_id}.`, LogLevel.ERROR, LogApp.PACKET_DATA_SERVER);
+    }
   }
+
   // get player inventory from database.
   const player = await User.findOne({ where: { token: packet.data.token } });
   // construct a JoinPacket
@@ -31,5 +31,13 @@ export default async function request_join_packet(packet: NetworkPacketData<Requ
     .build();
 
   // publish the packet
-  await join_packet.publish();
+  if (world.length) {
+    await join_packet.publish();
+  } else {
+    // note: this is not ideal, but it works for now.
+    setTimeout(() => {
+      // wait for rust process to start.
+      join_packet.publish();
+    }, 800);
+  }
 }
--- packages/database-redis/src/lib/service/world/world_service.ts
+++ packages/database-redis/src/lib/service/world/world_service.ts
@@ -2,10 +2,11 @@ import { LogApp, LogLevel, RedisWorld, TPS, log } from '@shared';
 import { exec } from 'child_process';
 import { cwd } from 'process';
 import { RedisClientType } from 'redis';
-import { World } from './world_utils';
+import { World as WorldUtils } from './world_utils';
+import { World } from '@virtcon2/database-postgres';
 
-const startWorldProcess = async (new_world: RedisWorld, redis: RedisClientType) => {
-  const world = await World.registerWorld(new_world, redis);
+const startWorldProcess = async (new_world: RedisWorld, redis: RedisClientType): Promise<number> => {
+  const world = await WorldUtils.registerWorld(new_world, redis);
   const worldProcess = exec(`$(which cargo) run`, {
     cwd: `${cwd()}/apps/packet_tick_server`,
     env: {
@@ -22,8 +23,20 @@ const startWorldProcess = async (new_world: RedisWorld, redis: RedisClientType) 
   });
 
   log(`Created world "${new_world.id}", --> process id: ${worldProcess.pid}`, LogLevel.OK, LogApp.SERVER);
+  return worldProcess.pid || 0;
 };
-
+const loadWorld = async (world_id: string): Promise<RedisWorld> => {
+  const world = await World.findOne({ where: { id: world_id } });
+  if (!world) {
+    throw new Error(`World ${world_id} does not exist.`);
+  }
+  return {
+    id: world.id,
+    players: [],
+    buildings: [],
+  } as RedisWorld;
+};
 export const worldService = {
   startWorldProcess,
+  loadWorld,
 };
