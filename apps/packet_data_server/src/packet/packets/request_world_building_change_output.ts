cf03938236ab4e3f2089b825a5cb95c22921fe21
Olle Thunberg <olle@fls.se>
2023-06-21 01:42:52 +0200

    Remove inventory packet response when requesting to chage world building output position

--- apps/packet_data_server/src/packet/packets/request_world_building_change_output.ts
+++ apps/packet_data_server/src/packet/packets/request_world_building_change_output.ts
@@ -1,10 +1,9 @@
-import { NetworkPacketDataWithSender, RequestWorldBuildingChangeOutput, RequestWorldBuildingPacket } from '@virtcon2/network-packet';
-import { RedisClientType } from 'redis';
-import request_player_inventory_packet from './request_player_inventory_packet';
-import request_world_building_packet from './request_world_building_packet';
+import { LogApp, LogLevel } from '@shared';
 import { WorldBuilding } from '@virtcon2/database-postgres';
-import { LogLevel, LogApp } from '@shared';
+import { NetworkPacketDataWithSender, RequestWorldBuildingChangeOutput, RequestWorldBuildingPacket } from '@virtcon2/network-packet';
 import { log } from 'console';
+import { RedisClientType } from 'redis';
+import request_world_building_packet from './request_world_building_packet';
 
 export default async function request_world_building_change_output(
   packet: NetworkPacketDataWithSender<RequestWorldBuildingChangeOutput>,
@@ -24,8 +23,6 @@ export default async function request_world_building_change_output(
   await WorldBuilding.update({ id: packet.data.building_id }, { output_pos_x: packet.data.output_pos_x, output_pos_y: packet.data.output_pos_y });
 
 
-  // send the updated inventory to the player and to the building
-  request_player_inventory_packet(packet, redisPubClient);
   const request_world_building_packet_data: NetworkPacketDataWithSender<RequestWorldBuildingPacket> = {
     packet_sender: packet.packet_sender,
     packet_type: packet.packet_type,
