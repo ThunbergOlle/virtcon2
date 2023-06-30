872dd45dc09dbd8971e6dc29517d213d99563bd6
Olle Thunberg <olle@fls.se>
2023-06-30 15:06:30 +0200

    Added synchronous packet management to avoid race conditions to some packet types

--- apps/packet_data_server/src/main.ts
+++ apps/packet_data_server/src/main.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
-import { DeconstructRedisPacket, RedisPacketPublisher } from '@virtcon2/network-packet';
+import { DeconstructRedisPacket, NetworkPacketData, RedisPacketPublisher } from '@virtcon2/network-packet';
 import dotenv from 'dotenv';
 import { cwd } from 'process';
 
 import { RedisClientType, createClient as createRedisClient } from 'redis';
-import packet_handler from './packet/packet_handler';
+import all_packet_handler, { sync_packet_handler } from './packet/packet_handler';
 import { AppDataSource } from '@virtcon2/database-postgres';
 import { LogApp, LogLevel, log } from '@shared';
 import { worldService } from '@virtcon2/database-redis';
@@ -20,11 +20,21 @@ redisPubClient.connect();
 redisSubClient.on('error', (err) => log(err, LogLevel.ERROR, LogApp.PACKET_DATA_SERVER));
 redisSubClient.connect();
 
+// Packet queue for handling packets in order.
+const packet_queue: NetworkPacketData<unknown>[] = [];
+
 redisSubClient.pSubscribe(`${RedisPacketPublisher.channel_prefix}*`, (message, channel) => {
-  // get world_id of t
   const deconstructed_packet = DeconstructRedisPacket<unknown>(message, channel);
-  packet_handler(deconstructed_packet, redisPubClient);
+  all_packet_handler(deconstructed_packet, redisPubClient, packet_queue);
 });
+const handle_queue = async () => {
+  if (packet_queue.length) {
+    const packet = packet_queue.shift();
+    await sync_packet_handler(packet, redisPubClient);
+  }
+  setTimeout(handle_queue, 0);
+};
+handle_queue();
 
 async function exit() {
   // remove all worlds from redis
--- apps/packet_data_server/src/packet/packet_handler.ts
+++ apps/packet_data_server/src/packet/packet_handler.ts
@@ -12,22 +12,20 @@ import {
   RequestWorldBuildingPacket,
 } from '@virtcon2/network-packet';
 import { RedisClientType } from 'redis';
-import request_player_inventory_packet from './packets/request_player_inventory_packet';
-import request_join_packet from './packets/request_join_packet';
 import request_destroy_resource_packet from './packets/request_destroy_resource_packet';
+import request_join_packet from './packets/request_join_packet';
 import request_place_building_packet from './packets/request_place_building_packet';
-import request_world_building_packet from './packets/request_world_building_packet';
-import request_move_inventory_item_packet from './packets/request_move_inventory_item_packet';
+import request_player_inventory_packet from './packets/request_player_inventory_packet';
 import request_world_building_change_output from './packets/request_world_building_change_output';
+import request_world_building_packet from './packets/request_world_building_packet';
 import internal_world_building_finished_processing_packet from './packets/internal_world_building_finished_processing_packet';
+import request_move_inventory_item_packet from './packets/request_move_inventory_item_packet';
 
-let is_busy: Promise<void> | null = null;
-
-export default async function packet_handler(packet: NetworkPacketData<unknown>, redisPubClient: RedisClientType) {
-  if (is_busy) {
-    await is_busy;
-    is_busy = null;
-  }
+export default function all_packet_handler(
+  packet: NetworkPacketData<unknown>,
+  redisPubClient: RedisClientType,
+  packet_queue: NetworkPacketData<unknown>[],
+) {
   switch (packet.packet_type) {
     case PacketType.REQUEST_PLAYER_INVENTORY: {
       request_player_inventory_packet(packet as NetworkPacketDataWithSender<RequestPlayerInventoryPacket>, redisPubClient);
@@ -50,18 +48,36 @@ export default async function packet_handler(packet: NetworkPacketData<unknown>,
       break;
     }
     case PacketType.REQUEST_MOVE_INVENTORY_ITEM: {
-      is_busy = request_move_inventory_item_packet(packet as NetworkPacketDataWithSender<RequestMoveInventoryItemPacketData>, redisPubClient);
+      packet_queue.push(packet);
+      // request_move_inventory_item_packet(packet as NetworkPacketDataWithSender<RequestMoveInventoryItemPacketData>, redisPubClient);
       break;
     }
     case PacketType.REQUEST_WORLD_BUILDING_CHANGE_OUTPUT: {
-      is_busy = request_world_building_change_output(packet as NetworkPacketDataWithSender<RequestWorldBuildingChangeOutput>, redisPubClient);
+      request_world_building_change_output(packet as NetworkPacketDataWithSender<RequestWorldBuildingChangeOutput>, redisPubClient);
       break;
     }
     case PacketType.INTERNAL_WORLD_BUILDING_FINISHED_PROCESSING: {
-      is_busy = internal_world_building_finished_processing_packet(packet as NetworkPacketData<InternalWorldBuildingFinishedProcessing>, redisPubClient);
+      packet_queue.push(packet);
+      // internal_world_building_finished_processing_packet(packet as NetworkPacketData<InternalWorldBuildingFinishedProcessing>, redisPubClient);
       break;
     }
     default: {
+      break;
+    }
+  }
+}
+
+export async function sync_packet_handler(
+  packet: NetworkPacketData<unknown>,
+  redisPubClient: RedisClientType,
+) {
+  switch (packet.packet_type) {
+    case PacketType.REQUEST_MOVE_INVENTORY_ITEM: {
+      await request_move_inventory_item_packet(packet as NetworkPacketDataWithSender<RequestMoveInventoryItemPacketData>, redisPubClient);
+      break;
+    }
+    case PacketType.INTERNAL_WORLD_BUILDING_FINISHED_PROCESSING: {
+      await internal_world_building_finished_processing_packet(packet as NetworkPacketData<InternalWorldBuildingFinishedProcessing>, redisPubClient);
       break;
     }
   }
