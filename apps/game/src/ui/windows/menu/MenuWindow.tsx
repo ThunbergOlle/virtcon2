5df08f33bf46626aae242c7f762e1971d9529b4b
Olle Thunberg <olle.thunberg03@gmail.com>
2024-03-29 23:45:57 +0100

    Remove junk code

--- apps/game/src/ui/windows/menu/MenuWindow.tsx
+++ apps/game/src/ui/windows/menu/MenuWindow.tsx
@@ -6,25 +6,18 @@ import { WindowManager, WindowType } from '../../lib/WindowManager';
 
 export default function MenuWindow(props: { windowManager: WindowManager }) {
   const navigate = useNavigate();
-  useEffect(() => {
-    events.subscribe('onPlayerMenuOpened', () => {
-      console.log('onPlayerMenuOpened');
-      props.windowManager.openWindow(WindowType.VIEW_MENU);
-    });
 
-    return () => {
-      events.unsubscribe('onPlayerMenuOpened', () => {});
-    };
-  }, []);
   const leaveWorld = () => {
     navigate('/');
   };
   return (
-    <Window windowManager={props.windowManager} title="Menu" width={800} height={800} defaultPosition={{ x: window.innerWidth / 2 - 400, y: 40 }} windowType={WindowType.VIEW_MENU}>
+    <Window title="Menu" width={800} height={800} defaultPosition={{ x: window.innerWidth / 2 - 400, y: 40 }} windowType={WindowType.VIEW_MENU}>
       <div className="flex flex-col h-full">
         <div className="flex-1 text-center flex flex-col w-[80%] m-auto space-y-5">
           <h2 className="text-2xl ">Menu</h2>
-          <button className="btn btn-primary" onClick={leaveWorld} >Leave world</button>
+          <button className="btn btn-primary" onClick={leaveWorld}>
+            Leave world
+          </button>
         </div>
       </div>
     </Window>
