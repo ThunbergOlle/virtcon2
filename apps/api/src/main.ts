2ba39e2d023f2d8fc1fe465f418df8ae3f969b0d
Olle Thunberg <olle.thunberg03@gmail.com>
2024-04-07 17:45:44 +0200

    Allow websocket connections

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,17 +1,19 @@
-import 'reflect-metadata';
 import { ApolloServer } from '@apollo/server';
 import { LogApp, LogLevel, log } from '@shared';
 import http from 'http';
+import 'reflect-metadata';
 
 import { expressMiddleware } from '@apollo/server/express4';
 import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { AppDataSource, setupDatabase } from '@virtcon2/database-postgres';
 import cors from 'cors';
 import express from 'express';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { buildSchema } from 'type-graphql';
+import { WebSocketServer } from 'ws';
 import { FormatGraphQLErrorResponse } from './graphql/FormatGraphQLErrorResponse';
 import { ContextMiddleware, RequestContext } from './graphql/RequestContext';
 import { resolvers } from './resolvers/resolvers';
-import { AppDataSource, setupDatabase } from '@virtcon2/database-postgres';
 
 log('Starting API', LogLevel.INFO, LogApp.API);
 
@@ -25,34 +27,66 @@ const port = 3000;
   log('Database initialized', LogLevel.INFO, LogApp.API);
 
   const app = express();
+
+  app.use(
+    cors({
+      methods: '*',
+      // origin: ['http://localhost:4200', 'http://localhost:3000', 'https://sandbox.embed.apollographql.com', 'https://studio.apollographql.com/'],
+      origin: '*',
+      allowedHeaders: '*',
+      credentials: true,
+    }),
+  );
+
   const httpServer = http.createServer(app);
 
+  const schema = await buildSchema({
+    resolvers: resolvers,
+    validate: {
+      forbidUnknownValues: false,
+    },
+  });
+
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql',
+  });
+
+  const serverCleanup = useServer({ schema }, wsServer);
+
   const apolloServer = new ApolloServer<RequestContext>({
-    schema: await buildSchema({
-      resolvers: resolvers,
-      validate: {
-        forbidUnknownValues: false,
+    schema: schema,
+    csrfPrevention: false,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
       },
-    }),
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    ],
     formatError: FormatGraphQLErrorResponse,
   });
+
   await apolloServer.start();
 
-  app.use(cors(
-    {methods: ['GET', 'POST'], origin: ['http://localhost:4200', 'http://localhost:3000']}
-  ));
-  app.use(express.json());
   app.use(
+    '/graphql',
+    express.json(),
     expressMiddleware(apolloServer, {
       context: ContextMiddleware,
     }),
   );
+
   app.get('/', (req, res) => {
     res.send({ message: 'Hello API' });
   });
 
-  app.listen(port, host, () => {
+  httpServer.listen(port, host, () => {
     log(`API service started running on http://${host}:${port}`, LogLevel.INFO, LogApp.API);
   });
 })();
