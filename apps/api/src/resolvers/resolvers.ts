23e9aac1b7f025b5a9ebe7468e5068aa865aa3b3
Olle Thunberg <olle.thunberg03@gmail.com>
2024-04-07 17:46:31 +0200

    Add world building resolver

--- apps/api/src/resolvers/resolvers.ts
+++ apps/api/src/resolvers/resolvers.ts
@@ -1,6 +1,7 @@
-import { NonEmptyArray } from "type-graphql";
-import { ItemResolver } from "./item/ItemResolver";
-import { UserResolver } from "./user/UserResolver";
+import { NonEmptyArray } from 'type-graphql';
+import { ItemResolver } from './item/ItemResolver';
+import { UserResolver } from './user/UserResolver';
+import { WorldBuildingResolver } from './worldBuilding/WorldBuildingResolver';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export const resolvers = [ItemResolver, UserResolver] as NonEmptyArray<Function> | NonEmptyArray<string>
+export const resolvers = [ItemResolver, UserResolver, WorldBuildingResolver] as NonEmptyArray<Function> | NonEmptyArray<string>;
---
+++ apps/api/src/resolvers/worldBuilding/WorldBuildingResolver.ts
@@ -0,0 +1,33 @@
+import { TOPIC_BUILDING_UPDATE, WorldBuilding } from '@virtcon2/database-postgres';
+import { withFilter } from 'graphql-subscriptions';
+import { Arg, ID, Query, Resolver, Subscription } from 'type-graphql';
+import { subscribe } from '../../service/RedisService';
+
+@Resolver()
+export class WorldBuildingResolver {
+  @Subscription(() => WorldBuilding, {
+    subscribe: withFilter(
+      (_, args) => subscribe.asyncIterator(`${TOPIC_BUILDING_UPDATE}.${args.id}`),
+      (payload, variables) => {
+        console.log(payload, variables);
+        return payload.world_building.id === variables.id;
+      },
+    ),
+  })
+  inspectWorldBuilding(
+    @Arg('id', () => ID, { nullable: false })
+    id: number,
+  ): Promise<WorldBuilding> {
+    console.log('inspectWorldBuilding', id);
+
+    return WorldBuilding.findOne({ where: { id } });
+  }
+
+  @Query(() => WorldBuilding)
+  async worldBuilding(
+    @Arg('id', () => ID, { nullable: false })
+    id: number,
+  ): Promise<WorldBuilding | undefined> {
+    return WorldBuilding.findOne({ where: { id } });
+  }
+}
